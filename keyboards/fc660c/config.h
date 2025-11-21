f3dfa8072eae686ffd26604e4702b01384da1504
Sebtoun <sebtoun@gmail.com>
2025-11-21 13:02:09 +0100

    [FC660C] revert customization of default vial keymaps

--- keyboards/fc660c/config.h
+++ keyboards/fc660c/config.h
@@ -44,5 +44,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // test all keys before further adjustment.
 // this should probably stay in the range +/-5.
 // #define ACTUATION_DEPTH_ADJUSTMENT 0
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
